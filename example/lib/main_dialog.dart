de377e4e9ff40dbd0c8fb05f2c5dfb4514fca4ce
muqaddas-moeen <muqaddasmoeen@gmailcom>
2025-03-25 10:55:39 +0500

    added package and modified accordingly

---
+++ example/lib/main_dialog.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(appBar: AppBar(title: Text('Demo')), body: MyHomePage()),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController controller = TextEditingController();
+  String initialCountry = 'PK';
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InternationalPhoneNumberInput(
+              onInputChanged: (PhoneNumber number) {
+                print(number.phoneNumber);
+              },
+              onInputValidated: (bool value) {
+                print(value);
+              },
+              ignoreBlank: true,
+              autoValidateMode: AutovalidateMode.disabled,
+              initialValue: PhoneNumber(isoCode: 'PK'),
+              textFieldController: controller,
+              inputBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  borderSide:
+                      BorderSide(color: Colors.black.withOpacity(0.09))),
+              selectorConfig: SelectorConfig(
+                selectorType: PhoneInputSelectorType.DIALOG,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                formKey.currentState?.validate();
+              },
+              child: Text('Validate'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                getPhoneNumber('+15417543010');
+              },
+              child: Text('Update'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getPhoneNumber(String phoneNumber) async {
+    PhoneNumber number =
+        await PhoneNumber.getRegionInfoFromPhoneNumber(phoneNumber, 'US');
+
+    String parsableNumber = await PhoneNumber.getParsableNumber(number);
+    controller.text = parsableNumber;
+
+    setState(() {
+      initialCountry = number.isoCode ?? '';
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
